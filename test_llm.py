59e5507f7831173f16d5adb348d77327075da55a
lkpanganiban <ian.panganiban@outlook.ph>
2023-09-25 13:41:53 +0800

    docs: added README.md and cleaned up to inline documentation in the notebook

--- test_llm.py
+++
@@ -1,6 +0,0 @@
-import os
-from llama_cpp import Llama
-
-llm = Llama(model_path=os.getenv("MODEL"))
-output = llm("Q: What is the smallest mammal? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
-print(output)
